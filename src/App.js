e46bde6412a27d051058cfbd0b9dc7b5579f70a6
fujita1000 <fujitanao.rmt@gmail.com>
2023-12-21 23:13:38 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link
+} from 'react-router-dom';
+import Home from './components/Home';
+import One from './components/One';  
+import Two from './components/Two';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <nav>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/one">Page One</Link></li>
+              <li><Link to="/two">Page Two</Link></li>
+            </ul>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/one" element={<One />} />  
+          <Route path="/two" element={<Two />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/One.js
@@ -0,0 +1,12 @@
+// components/One.js
+import React from 'react';
+
+const One = () => {
+  return (
+    <div>
+      <h2>Page One</h2>
+    </div>
+  );
+};
+
+export default One;
